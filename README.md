02214e6c6c482fc82797b43488eb36e87178eae4
Nikita <b00men.pochta@gmail.com>
2016-01-19 01:49:55 +0600

    Work with installation script. Now only amd64 debian8

--- README.md
+++ README.md
@@ -7,23 +7,7 @@ mb is a message board on cppcms
 
 install [libgcrypt11](https://packages.debian.org/wheezy/libgcrypt11) and [libicu48](https://packages.debian.org/wheezy/libicu48>) manual (for apt.cppcms.com)
 
-    wget http://ftp.fr.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u3_amd64.deb
-    wget http://ftp.fr.debian.org/debian/pool/main/i/icu/libicu48_4.8.1.1-12+deb7u3_amd64.deb
-    dpkg -i libgcrypt11_1.5.0-5+deb7u3_amd64.deb libicu48_4.8.1.1-12+deb7u3_amd64.deb
-
-Then exicute:
-
-    echo "deb http://apt.cppcms.com/ wheezy main" > /etc/apt/sources.list.d/cppcms.list
-    apt-get update
-    apt-get install git nginx cmake g++ libcppcms-dev libcppdb-dev mysql-server libcppdb-mysql0 libboost-dev libmagick++-dev libboost-regex-dev gettext spawn-fcgi
-    git clone https://github.com/b00men/mb.git
+    sudo apt-get update && sudo apt-get install -y git
+    git clone https://github.com/b00men/mb
     cd mb
-    cmake .
-    make
-    make install
-    cp /usr/local/share/mb/config.js.sample /usr/local/share/mb/config.js
-
-cppcms_make_key --hmac sha1 --cbc aes
-echo "CREATE DATABASE IF NOT EXISTS mb" | mysql -u root -p
-mysql -u root -p mb < mysql.sql
-ln -s /etc/nginx/sites-available/message_board /etc/nginx/sites-enabled/message_board
+    sudo ./fast_install.sh
--- fast_install.sh
+++ fast_install.sh
@@ -1,36 +1,53 @@
-#/bin/bash
-if [[ `whoami` != "root" ]]
+#!/usr/bin/env bash
+if [ `whoami` != "root" ]
 then
 	echo You need to be root to perform this command. 
 	exit 1
 fi
-wget http://ftp.fr.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u3_amd64.deb
-wget http://ftp.fr.debian.org/debian/pool/main/i/icu/libicu48_4.8.1.1-12+deb7u3_amd64.deb
-dpkg -i libgcrypt11_1.5.0-5+deb7u3_amd64.deb libicu48_4.8.1.1-12+deb7u3_amd64.deb
+ARCH=`uname -r | sed 's/.*-//'`
+echo "Download and manual install libgcrypt11 and libicu48:"
+wget -q http://ftp.fr.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u3_$ARCH.deb
+wget -q http://ftp.fr.debian.org/debian/pool/main/i/icu/libicu48_4.8.1.1-12+deb7u3_$ARCH.deb
+dpkg -i libgcrypt11_1.5.0-5+deb7u3_$ARCH.deb libicu48_4.8.1.1-12+deb7u3_$ARCH.deb
+echo
+
+echo "Add apt.cppcms.com to sources and install dependets packeges:"
 echo "deb http://apt.cppcms.com/ wheezy main" > /etc/apt/sources.list.d/cppcms.list
-apt-get update
-apt-get -y install git nginx cmake g++ libcppcms-dev libcppdb-dev mysql-server libcppdb-mysql0 libboost-dev libmagick++-dev libboost-regex-dev gettext spawn-fcgi
-mkdir build
+apt-get update -qq
+apt-get install -y --force-yes -qq git nginx cmake g++ libcppcms-dev libcppdb-dev mysql-server libcppdb-mysql0 libboost-dev libmagick++-dev libboost-regex-dev gettext spawn-fcgi
+echo
+
+echo "Build and install message board:"
+mkdir -p build
 cd build
 cmake ..
 make
 make install
+echo
 
+
+echo "Configure MySQL."
 cd ../model
-echo "Prepare to create database."
-echo "MySQL root passwd:"
+echo "MySQL root passwd for database creating:"
 echo "CREATE DATABASE IF NOT EXISTS mb" | mysql -u root -p && echo "Database create."
 mysql -u root -p mb < mysql.sql
+echo
+
+echo "Copy site tamplate for nginx:"
 cp /usr/local/share/mb/message_board /etc/nginx/sites-available/message_board
 ln -s /etc/nginx/sites-available/message_board /etc/nginx/sites-enabled/message_board
-service nginx restart
+echo "Reload nginx:"
+service nginx reload
+echo
+
+echo "Configure message board:"
 cp /usr/local/share/mb/config.js.sample /usr/local/share/mb/config.js
 cat /usr/local/share/mb/config.js | grep -B 1000 "\"hmac_key\"" | grep -v -e "\"hmac_key\"" -e "\"cbc_key\"" > /tmp/config.js
 cppcms_make_key --hmac sha1 --cbc aes | grep key >> /tmp/config.js
 cat /usr/local/share/mb/config.js | grep -A 1000 "\"hmac_key\"" | grep -v -e "\"hmac_key\"" -e "\"cbc_key\"" >> /tmp/config.js
 if [ "`cat /tmp/config.js | wc -l`" != 2 ]
 then
-	echo "cbc & hmac keys successfull generated!"
+	echo "cbc & hmac keys successfull generated."
 	cat /tmp/config.js > /usr/local/share/mb/config.js
 	rm /tmp/config.js
 else
@@ -38,12 +55,13 @@ else
 	rm /tmp/config.js
 fi
 
-echo "MySQL database name:"
+echo -n "MySQL database name:"
 read DATABASE; echo
-echo "MySQL login"
+echo -n "MySQL login"
 read USER; echo
-echo "MySQL password:"
+echo -n "MySQL password:"
 read -s PASSWD; echo
-echo "$DATABASE $USER $PASSWD"
 sed -i -e "s/name_of_datebase/$DATABASE/" -e "s/user_of_datebase/$USER/" -e "s/password_of_database/$PASSWD/" /usr/local/share/mb/config.js
 sed -i 's/\/\/\"connection_string\" \: \"mysql/\"connection_string\" \: \"mysql/' /usr/local/share/mb/config.js
+echo
+
